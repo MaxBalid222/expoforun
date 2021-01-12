11b786635129265abd1cc825e1bbeda0ec180076
MaxBalid222 <76619002+MaxBalid222@users.noreply.github.com>
2021-01-12 23:58:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+$(function () {
+    new WOW().init();
+
+    $(document).ready(function(){
+
+
+
+        let headerBox = new TimelineMax();
+
+        new ScrollMagic.Scene({
+            // duration: 100,
+            triggerElement: $('header')[0], 
+            offset: wh*0.6
+        })
+        .setTween(headerBox)
+
+
+
+
+
+        $('.img__search',).click(function(event){
+            $('.header__search-input').toggleClass('header__search-input-activ');
+            $('.header__search-button').toggleClass('activ');
+            $('.img__search').toggleClass('no-activ');
+        });
+
+        $('.header__search-button',).click(function(event){
+            $('.header__search-input').toggleClass('header__search-input-activ');
+            $('.header__search-button').toggleClass('activ');
+            $('.img__search').toggleClass('no-activ');
+        });
+
+        $('.header__nav-bottom',).click(function(event){
+            $('.header__nav').toggleClass('header__nav-activ');
+            $('.header__nav-link').toggleClass('header__nav-link-activ');
+            $('body').toggleClass('lock');
+        });
+
+    });
+
+
+
+    $('.footer__item-title').on('click', function () {
+
+        $('.footer__item-list').removeClass('footer__item-list-activ');
+        $(this).parent().toggleClass('footer__item-list-activ');
+    
+    });
+
+});
+
